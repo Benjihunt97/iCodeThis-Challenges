b42ab38814700d616d7659ed3209dee4058cb27c
Benjihunt97 <73445088+Benjihunt97@users.noreply.github.com>
2024-05-29 13:16:26 +0100

    Update app.js

--- Scoreboard/app.js
+++ Scoreboard/app.js
@@ -55,6 +55,30 @@ data.forEach((item, index) => {
   board.appendChild(boardItem);
 });
 
+const firstPlace = board.children[0].querySelector('.trophy');
+  firstPlace.innerHTML = `
+      <svg width="22" height="22" viewBox="0 0 24 24" stroke-width="1.5" stroke="gold" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <path d="M17 3a1 1 0 0 1 .993 .883l.007 .117v2.17a3 3 0 1 1 0 5.659v.171a6.002 6.002 0 0 1 -5 5.917v2.083h3a1 1 0 0 1 .117 1.993l-.117 .007h-8a1 1 0 0 1 -.117 -1.993l.117 -.007h3v-2.083a6.002 6.002 0 0 1 -4.996 -5.692l-.004 -.225v-.171a3 3 0 0 1 -3.996 -2.653l-.003 -.176l.005 -.176a3 3 0 0 1 3.995 -2.654l-.001 -2.17a1 1 0 0 1 1 -1h10zm-12 5a1 1 0 1 0 0 2a1 1 0 0 0 0 -2zm14 0a1 1 0 1 0 0 2a1 1 0 0 0 0 -2z" stroke-width="0" fill="gold" />
+  </svg>
+    `;
+    
+  const secondPlace = board.children[1].querySelector('.trophy');
+  secondPlace.innerHTML = `
+      <svg width="22" height="22" viewBox="0 0 24 24" stroke-width="1.5" stroke="gold" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <path d="M17 3a1 1 0 0 1 .993 .883l.007 .117v2.17a3 3 0 1 1 0 5.659v.171a6.002 6.002 0 0 1 -5 5.917v2.083h3a1 1 0 0 1 .117 1.993l-.117 .007h-8a1 1 0 0 1 -.117 -1.993l.117 -.007h3v-2.083a6.002 6.002 0 0 1 -4.996 -5.692l-.004 -.225v-.171a3 3 0 0 1 -3.996 -2.653l-.003 -.176l.005 -.176a3 3 0 0 1 3.995 -2.654l-.001 -2.17a1 1 0 0 1 1 -1h10zm-12 5a1 1 0 1 0 0 2a1 1 0 0 0 0 -2zm14 0a1 1 0 1 0 0 2a1 1 0 0 0 0 -2z" stroke-width="0" fill="#94A3B8" />
+  </svg>
+    `;
+    
+  const thirdPlace = board.children[2].querySelector('.trophy');
+  thirdPlace.innerHTML = `
+        <svg width="22" height="22" viewBox="0 0 24 24" stroke-width="1.5" stroke="gold" fill="none" stroke-linecap="round" stroke-linejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M17 3a1 1 0 0 1 .993 .883l.007 .117v2.17a3 3 0 1 1 0 5.659v.171a6.002 6.002 0 0 1 -5 5.917v2.083h3a1 1 0 0 1 .117 1.993l-.117 .007h-8a1 1 0 0 1 -.117 -1.993l.117 -.007h3v-2.083a6.002 6.002 0 0 1 -4.996 -5.692l-.004 -.225v-.171a3 3 0 0 1 -3.996 -2.653l-.003 -.176l.005 -.176a3 3 0 0 1 3.995 -2.654l-.001 -2.17a1 1 0 0 1 1 -1h10zm-12 5a1 1 0 1 0 0 2a1 1 0 0 0 0 -2zm14 0a1 1 0 1 0 0 2a1 1 0 0 0 0 -2z" stroke-width="0" fill="#7D2D12" />
+    </svg>
+      `;
+
 const boardItems = document.querySelectorAll('.board-item');
 
 boardItems.forEach(item => {
